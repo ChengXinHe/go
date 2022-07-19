772b1887471186f47c3f34bd1d0f9b9a14aca1bb
Cy <xc2283@nyu.edu>
2022-07-18 21:36:47 -0400

    7.28

--- struct_methods.go
+++ struct_methods.go
@@ -14,5 +14,14 @@ func (stu *Student) hello(person string) string {
 }
 
 func main() {
-	stu
+	//字段不一定都要赋值，可以被赋予默认值
+	stu := &Student{
+		name: "Tom",
+		age:  18,
+	}
+	msg := stu.hello("JAck")
+	fmt.Println(msg)
+	//stu是实例名，*Student是类型。可以通过实例名访问字段name和方法
+	stu2 := new(Student)
+	fmt.Println(stu2.hello("alice"))
 }
