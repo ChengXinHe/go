09301ce009c76a4f2624c00705a0aa081745ac76
Cy <xc2283@nyu.edu>
2022-07-20 09:32:42 -0400

    7.20

---
+++ lru.go
@@ -0,0 +1,20 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	l1        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
